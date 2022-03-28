4a13a582c4e4c8849855f3164e7e1c66f2f01c08
unknown <thanhhuypham194@gmail.com>
2022-03-28 11:08:17 +0700

    bt_1

---
+++ src/Component/Headers.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+
+export default class extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container px-lg-5">
+          <a className="navbar-brand" href="#!">
+            Start Bootstrap
+          </a>
+          <div>
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#!">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#!">
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#!">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
